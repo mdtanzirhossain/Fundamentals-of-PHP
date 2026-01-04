5d57517c5c20c263961a1d186455c85aeb4f06be
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2026-01-04 22:00:40 +0600

    form handling part 2

--- admission/form.php
+++ admission/form.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <h1>Admission Form</h1>
-    <form action ="save.php" method = "post">
-        <label for="username">Username</label>
+    <form action ="save.php" method = "post" enctype="multipart/form-data">
+        <!-- <label for="username">Username</label>
         <input type="text" name="username" id=""></br>
         <label for="email">User Email</label>
         <input type="email" name="email" id=""></br>
@@ -22,8 +22,38 @@
         <input type="radio" name="gender" value="male" id="">Male
         <input type="radio" name="gender" value="female" id="">Female
         <input type="radio" name="gender" value="other" id="">Other
+        </br>
 
+        <label for="bestTeacher">Best Teacher</label></br> -->
+        <!-- The values go as array to output -->
+        <!-- <input type="checkbox" name="bestTeacher[]" value="Hadi Zaman" id=""> Hadi Zaman</br>
+        <input type="checkbox" name="bestTeacher[]" value="Sumit Saha" id=""> Sumit Saha</br>
+        <input type="checkbox" name="bestTeacher[]" value="Rabbil Hasan" id=""> Rabbil Hasan</br>
+        <label for="bestTeacher">Course Fee</label></br>
+        <input type="number" name="course_fee" id=""></br>
+        <label for="bestTeacher">Contact No: </label></br>
+        <input type="tel" name="cellnumber" id=""></br>
+
+        <label for="birthDate">Birthday</label>
+        <input type="date" name="birthDate" id=""> -->
+
+        <!-- <label for="color">Choose Your Best Color</label>
+        <input type="color" name="color" id=""></br>
+        <label for="time"></label></br>
+        <input type="time" name="time" id=""></br>
+        <label for="url"></label></br>
+        <input type="url" name="url" id=""> -->
+
+        <label for="file">Upload Image</label></br>
+        <!-- <input type="image" src="" alt=""> -->
+
+        <input type="file" name="image" id=""> </br>
+<!-- image is amultidimensional array. echo will not work on it -->
+        <label for="month">Month</label></br>
+        <input type="month" name="month" id=""></br>
+        <input type="hidden" name="ip" value="<?php echo $_SERVER['REMOTE_PORT']?>">
         <button type="submit">Submit</button>
+        <input type="reset" value="reset">
     </form>
 </body>
 </html>
--- admission/save.php
+++ admission/save.php
@@ -1,14 +1,34 @@
 <?php
 
-$username = $_POST["username"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$shortDescription = $_POST["shortDescription"];
-$gender = $_POST["gender"];
-echo $username."</br>";
-echo $email."</br>";
-echo $password."</br>";
-echo $shortDescription."</br>";
-echo $gender;
+// $username = $_POST["username"];
+// $email = $_POST["email"];
+// $password = $_POST["password"];
+// $shortDescription = $_POST["shortDescription"];
+// $gender = $_POST["gender"];
+// echo $username."</br>";
+// echo $email."</br>";
+// echo $password."</br>";
+// echo $shortDescription."</br>";
+// echo $gender;
 
+// $bestTeacher = $_POST["bestTeacher"];
+// $course_fee = $_POST["course_fee"];
+// $cellnumber = $_POST["cellnumber"];
+// $birthDate = $_POST["birthDate"];
+// $color = $_POST["color"];
+// $time = $_POST["time"];
+// $url = $_POST["url"];
+// $image = $_FILES['image'];
+// $month = $_POST['month'];
+$ip = $_POST['ip'];
+// echo $bestTeacher; is not applicablen because it does not work on array
+// var_dump($bestTeacher)."</br>";
+// echo "</br>".$course_fee."</br>";
+// echo $cellnumber."</br>";
+// echo $birthDate."</br>";
+// echo $color."</br>";
+// echo $time."</br>";
+// echo $url."</br>";
+// var_dump($image)."</br>";
+echo $ip;
 ?>
