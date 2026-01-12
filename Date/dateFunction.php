3b636ce85f31d77bb88fd64d04348e50c2f9d958
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2026-01-12 22:58:07 +0600

    date

---
+++ Date/dateFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+// date(formate, timestamp)
+
+echo date("d")."</br>";
+echo date("D")."</br>";
+echo date("m")."</br>";
+echo date("M")."</br>";
+echo date("y")."</br>";
+echo date("Y")."</br>";
+echo date("y/m/d")."</br>"; // For date
+echo date("Y/M/D")."</br>"; // not for date
