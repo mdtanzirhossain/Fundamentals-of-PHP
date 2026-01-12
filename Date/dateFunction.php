8b1df6b63d8c52e0b85e0799af8d31c87cbba5a2
mdtanzirhossain <mdtanzirhossainm@gmail.com>
2026-01-12 23:30:39 +0600

    copyright

--- Date/dateFunction.php
+++ Date/dateFunction.php
@@ -10,3 +10,18 @@ echo date("y")."</br>";
 echo date("Y")."</br>";
 echo date("y/m/d")."</br>"; // For date
 echo date("Y/M/D")."</br>"; // not for date
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- Copyright -->
+
+&copy; 2006-<?php echo date("Y");?>
+</body>
+</html>
